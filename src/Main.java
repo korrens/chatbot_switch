286848f3049483a409b439130b1a80e64721a8b1
Reinis <103788853+korrens@users.noreply.github.com>
2022-05-24 14:45:59 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Hello there! It's Chatbot here. How can I help you?");
+        boolean running = true;
+        while (running == true) {
+            System.out.println(" ");
+            String currentLine = input.nextLine();
+            switch(currentLine) {
+                case "What you can?":
+                    System.out.println("Nice question. Try asking me what made me and what I eat!");
+                    break;
+                case "What do you eat?":
+                    System.out.println("I eat only your battery and internet!");
+                    break;
+                case "Who made you?":
+                    System.out.println("I was made thanks to Reinis!");
+                    break;
+                case "Bye":
+                    System.out.println("Bye! Have a nice day!");
+                    break;
+                default:
+                    System.out.println("Sorry! I am new to this and didn't understand. Try to ask me in a different way!");
+                    break;
+            }
+        }
     }
 }
